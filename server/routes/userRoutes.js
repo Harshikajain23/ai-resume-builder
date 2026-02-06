7cfd40d6249866e9dac953aa37bb02e5782dd7f2
Harshikajain23 <jainharshika2002@gmail.com>
2026-02-06 19:49:32 +0530

    API created for user authentication

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import { getUserByID, loginUser, regsiterUser } from '../Controllers/User.Controller.js';
+import protect from '../middlewares/auth.Middleware.js';
+
+const userRouter = express.Router();
+
+userRouter.post('/register', regsiterUser);
+userRouter.post('/login', loginUser);
+userRouter.get('/data', protect, getUserByID);
+
+export default userRouter;
