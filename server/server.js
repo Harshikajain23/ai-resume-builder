75c1eec914c35153d9169b948553f5fef2892666
Harshikajain23 <jainharshika2002@gmail.com>
2026-02-13 19:25:45 +0530

    again trying on render

--- server/server.js
+++ server/server.js
@@ -20,6 +20,6 @@ app.use('/api/users', userRouter)
 app.use('/api/resumes', resumeRouter)
 app.use('/api/ai', aiRouter)
 
-app.listen(PORT, ()=> {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server is running on port ${PORT}`);
-})
+});
