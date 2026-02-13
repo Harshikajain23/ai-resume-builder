77f4030515a61b6d843776efb53b8331e49b9726
Harshikajain23 <jainharshika2002@gmail.com>
2026-02-13 18:02:20 +0530

    Final

--- server/routes/resumeRoutes.js
+++ server/routes/resumeRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import protect from '../middlewares/auth.Middleware.js';
-import { createResume, deleteResume, getPublicResumeById, getResumeById, updateResume } from '../Controllers/ResumeController.js';
+import { createResume, deleteResume, getPublicResumeById, getResumeById, updateResume } from '../Controllers/resumeController.js';
 import upload from '../configs/multer.js';
 
 const resumeRouter = express.Router();
